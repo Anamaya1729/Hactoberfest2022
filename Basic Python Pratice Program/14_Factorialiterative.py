594d1d3710bcb860138ca5e2ec1a621e3a85e168
anamaya <sharma.011999@gmail.com>
2022-09-26 13:36:30 +0530

    Closes #13 Factorial Using Iterative Method

--- Basic Python Pratice Program/14_Factorialiterative.py
+++ Basic Python Pratice Program/14_Factorialiterative.py
@@ -1,26 +1,24 @@
+""" This is a program to calculate the factorial of a number using a for loop and a while loop. """
 
-def fact_iterative(n):
-    facto=1
-    if(n==0):
-        # print("Factorial of 0 is 1")
-        return 1
-    else:
-        for i in range(1,n+1):
-            facto=facto*i
-        return facto
-        # print("Factorial of ",n," is :",facto )
-        
-def fact_recursive(n):
-    
-    if(n==0):
-        # print("Factorial of 0 is 1")
-        return 1
-    else:
-         
-        return n*fact_recursive(n-1)
-        # print("Factorial of ",n," is :",facto )
+def factorial_for(n):
+    """ This function calculates the factorial of a number using a for loop. """
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
-print(fact_iterative(3))
-print(fact_iterative(3))
+def factorial_while(n):
+    """ This function calculates the factorial of a number using a while loop. """
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
 
+def main():
+    """ This is the main function. """
+    print(f"factorial_for(5) = {factorial_for(5)}")
+    print(f"factorial_while(5) = {factorial_while(5)}") 
 
+if __name__ == "__main__":
+    main()
