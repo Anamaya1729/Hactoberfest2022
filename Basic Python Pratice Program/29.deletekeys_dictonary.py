c20fd6f9da1b010c6f1cc26c9a20fca991d40425
roysuraj123 <suraj.roy@cloudcraftz.io>
2022-10-20 17:32:37 +0530

    dictonary keys delete added

---
+++ Basic Python Pratice Program/29.deletekeys_dictonary.py
@@ -0,0 +1,12 @@
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+for k in keys:
+    sample_dict.pop(k)
+print(sample_dict)
