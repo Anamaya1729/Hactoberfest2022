c71e0722d7f2d7d6180270d77b676d12e6624d45
MohammadAnas-hub <anasmohammad557@gmail.com>
2022-10-23 15:25:25 +0530

    Update 9_Fibonacci_series.py

--- Basic Python Pratice Program/9_Fibonacci_series.py
+++ Basic Python Pratice Program/9_Fibonacci_series.py
@@ -1,5 +1,5 @@
 fibonacciSeries = [0,1]
-
+n = int(input("Input the number of elements in fibonacci series - "))
 def fibSer(n):
     if n<0:
         return "error"
@@ -8,5 +8,5 @@ def fibSer(n):
         while n != i:
             fibonacciSeries.append(fibonacciSeries[i-1] + fibonacciSeries[i-2])
             i += 1
-fibSer(5)
-print(fibonacciSeries)
+fibSer(10)
+print(f"First {n} members of fibonacci series are : ", fibonacciSeries)
