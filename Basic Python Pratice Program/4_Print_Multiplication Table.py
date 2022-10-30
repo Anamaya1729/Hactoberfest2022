0449e8b02a22b4a2535a63734de7840f9c933a6f
sreedevi <42407419+Sreedevi179@users.noreply.github.com>
2022-10-31 01:03:57 +0530

    Update 4_Print_Multiplication Table.py

--- Basic Python Pratice Program/4_Print_Multiplication Table.py
+++ Basic Python Pratice Program/4_Print_Multiplication Table.py
@@ -10,3 +10,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+""" Multiplication table """
+number = int(input ("Enter the number of which the user wants to print the multiplication table: "))      
+count = 1    
+
+print ("The Multiplication Table of: ", number)    
+while count <= 10:    
+    number = number * 1    
+    print (number, 'x', i, '=', number * count)    
+    count += 1    
